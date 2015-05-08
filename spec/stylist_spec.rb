850e099ffa9e0479009ecde67d83309ca1c73fb4
Guest User <jfairless@spectrumhomeservices.com>
2015-05-08 09:25:00 -0700

    restart to change language preference.

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -21,4 +21,18 @@ describe(Stylist) do
       expect(Stylist.all()).to(eq([]))
     end
   end
+
+  describe('.find') do
+    it('will return a stylist by its id number') do
+      test_stylist = Stylist.new({:name => 'Fifi', id => nil})
+      test_stylist.save()
+      test_stylist2 = Stylist.new({:name => 'Pierre', id => nil})
+      test_stylist2.save()
+      expect(Stylist.find(test_stylist2.id())).to(eq(test_stylist2)
+    end
+  end
+
+  describe('£')
+
+
 end
